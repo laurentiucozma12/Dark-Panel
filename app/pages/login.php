fbc7adbb2205aa157ead77f62836394cbf8c5f8f
Lau <laurentiucozma12@gmail.com>
2022-10-08 14:22:44 +0300

    v0.0.6

--- app/pages/login.php
+++ app/pages/login.php
@@ -24,28 +24,30 @@
 </head>
 <body>
 <main>
-<div class="wrap d-flex justify-content-center">
-    <div class="wrap col-7 col-sm-6 col-md-5 col-lg-4 col-xl-3 col-xxl-3">
-    <form>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password">
-            <div id="passwordHelpBlock" class="form-text">
-                Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+<div class="wrap">
+    <div class="container d-flex justify-content-center">
+        <div class="wrap col-12">
+        <h1>Login</h1>
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
-        </div>
-        
-        <button type="submit" class="greenBtn">Submit</button>
-        <br>
-        <div class="otherPage">
-            <a href="<?php echo WEB; ?>/app/pages/signup.php">Dont have an account? Go to signup.</a>
-        </div>
-    </form>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password">
+                <div id="passwordHelpBlock" class="form-text">
+                    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                </div>
+            </div>
+            <button type="submit" class="greenBtn">Submit</button>
+            <br>
+            <div class="otherPage">
+                <a href="<?php echo WEB; ?>/app/pages/signup.php">Dont have an account? Go to signup.</a>
+            </div>
+        </form>
+        </div>        
     </div>
 </div>
 </main>
