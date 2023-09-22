ee329a4de1e7aecf7fa0e47a399cdda1076fbb37
Lau <laurentiucozma12@gmail.com>
2023-09-22 23:40:51 +0300

    structure for Menu Large/Small screens implemented

--- app/pages/dashboard.php
+++ app/pages/dashboard.php
@@ -1,5 +1,5 @@
 <?php include '../../app/config/config.php';?>
-<?php include ROOT_PATH.'/app/config/connect.php';?>
+<?php include '../../app/config/connect.php';?>
 
 <!DOCTYPE html>
 <html>
@@ -7,11 +7,11 @@
     <meta charset="utf-8">
     <title>Dark Panel</title>
     <!-- CSS -->
-    <link href="<?php echo WEB_PATH; ?>/assets/css/style.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo WEB_PATH; ?>/assets/css/font.css" rel="stylesheet" type="text/css">
+    <link href="../../assets/css/style.css" rel="stylesheet" type="text/css">
+    <link href="../../assets/css/font.css" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap CSS -->
-    <link href="<?php echo WEB_PATH; ?>/assets/css/bootstrap-v5.0.2.css" rel="stylesheet"></head>
+    <link href="../../assets/css/bootstrap-v5.0.2.css" rel="stylesheet"></head>
 
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -25,7 +25,7 @@
 <main>
 
 <!-- Side-navbar -->
-<?php include ROOT_PATH."/assets/elements/sidebar.php" ?>
+<?php include "../../assets/elements/sidebar.php" ?>
 
 <!-- Content -->
 <div class="content active-cont">
@@ -45,7 +45,7 @@
                 <a href=""><i class="fa-regular fa-bell"></i></a>
             </div>            
             <div class="profile">
-                <img src="<?php echo WEB_PATH; ?>/assets/img/avatar.png" alt="profile img" class="profileImg">
+                <img src="../../assets/img/avatar.png" alt="profile img" class="profileImg">
             </div>
         </div>  
     </div>    
@@ -63,7 +63,7 @@
     <div class="middleBottomContainer row d-flex justify-content-lg-between justify-content-md-around gap-lg-4 gap-md-4 gap-sm-4 gap-4">
         <div class="container col-lg-2 col-md-5 col-sm-12 col-12">
             <div class="firstContainer">
-                <img src="<?php echo WEB_PATH; ?>/assets/img/avatar.png" alt="profile img" class="profileImg">
+                <img src="../../assets/img/avatar.png" alt="profile img" class="profileImg">
                 <h5>PHP Developer <br><span>124 Videos</span></h5>
             </div>
             <a href ="" class="secondContainer">
@@ -73,7 +73,7 @@
         </div>  
         <div class="container col-lg-2 col-md-5 col-sm-12 col-12">
             <div class="firstContainer">
-                <img src="<?php echo WEB_PATH; ?>/assets/img/avatar.png" alt="profile img" class="profileImg">
+                <img src="../../assets/img/avatar.png" alt="profile img" class="profileImg">
                 <h5>Python Programming <br><span>234 Videos</span></h5>
             </div>
             <a href ="" class="secondContainer">
@@ -83,7 +83,7 @@
         </div>   
         <div class="container col-lg-2 col-md-5 col-sm-12 col-12">
             <div class="firstContainer">
-                <img src="<?php echo WEB_PATH; ?>/assets/img/avatar.png" alt="profile img" class="profileImg">
+                <img src="../../assets/img/avatar.png" alt="profile img" class="profileImg">
                 <h5>Figma Design <br><span>87 Videos</span></h5>
             </div>
             <a href =""  class="secondContainer">
@@ -93,7 +93,7 @@
         </div>   
         <div class="container col-lg-2 col-md-5 col-sm-12 col-12">
             <div class="firstContainer">
-                <img src="<?php echo WEB_PATH; ?>/assets/img/avatar.png" alt="profile img" class="profileImg">
+                <img src="../../assets/img/avatar.png" alt="profile img" class="profileImg">
                 <h5>UI with Sketch <br><span>29 Videos</span></h5>
             </div>
             <a href =""  class="secondContainer">
@@ -103,7 +103,7 @@
         </div>   
         <div class="container col-lg-2 col-md-5 col-sm-12 col-12">
             <div class="firstContainer">
-                <img src="<?php echo WEB_PATH; ?>/assets/img/avatar.png" alt="profile img" class="profileImg">
+                <img src="../../assets/img/avatar.png" alt="profile img" class="profileImg">
                 <h5>Gruntt <br><span>18 Videos</span></h5>
             </div>
             <a href =""  class="secondContainer">
@@ -188,16 +188,16 @@
     <!-- <div class="copyright">© 2022 Copyright of Laurentiu Ioan Cozma</div> -->
 </footer>
 <!-- Ajax jQuery -->
-<script src="<?php echo WEB_PATH; ?>/assets/js/jQuery-v3.6.0.js"></script>
+<script src="../../assets/js/jQuery-v3.6.0.js"></script>
 
 <!-- Bootstrap JS -->
-<script src="<?php echo WEB_PATH; ?>/assets/js/bootstrap-v5.0.2.js"></script>
+<script src="../../assets/js/bootstrap-v5.0.2.js"></script>
 
 <!-- ChartJs -->
-<script src="<?php echo WEB_PATH; ?>/assets/js/chartJs-v3.9.1.js"></script>
+<script src="../../assets/js/chartJs-v3.9.1.js"></script>
 
 <!-- Functions -->
-<script src="<?php echo WEB_PATH; ?>/assets/js/functions.js"></script>
+<script src="../../assets/js/functions.js"></script>
 
 </body>
 </html>
